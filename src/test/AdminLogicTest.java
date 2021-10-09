15d0e4eeef45ea3cbe9ead413ab6cf33bc09da4f
a7m2c <a7m2c@host.docker.internal>
2021-10-09 09:24:18 +0900

    これが最新_20211009

--- src/test/AdminLogicTest.java
+++ src/test/AdminLogicTest.java
@@ -7,15 +7,15 @@ public class AdminLogicTest {
   public static void main(String[] args) {
     testExecute1();
   }
-  
+
   public static void testExecute1() {
     ItemBean itemNew = new ItemBean("tie0003", "ネクタイ", 12000, 3);
     ItemDAO dao = new ItemDAO();
-    boolean isNew = dao.newItemInfo(itemNew);
-    if (isNew) {
+    String item_id = dao.newItemInfo(itemNew);
+    if (item_id != null) {
       System.out.println("testExecute：成功しました");
     } else {
       System.out.println("testExecute：しっぱい");
-    } 
+    }
   }
 }
